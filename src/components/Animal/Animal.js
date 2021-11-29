0ed316100ecbe49754281c11c8b8d0cfba0fb96f
karyssa-dandrea <karyssadandrea@gmail.com>
2021-11-29 15:18:06 -0800

    added react code to main and animal.js

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return (
+    <>
+      <div className="Animal">
+        <img
+          height="100"
+          src={`${process.env.PUBLIC_URL}/animals/$ 
+        {props.type}.svg`}
+        />
+        <span>{props.name}</span>
+      </div>
+    </>
+  )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import './Main.css'
+import { animals } from '../../data.js'
+import backgroundImg from '../../background.png'
+import Animal from '../Animal/Animal.js'
+
+const animalArr = animals
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {animalArr.map((item) => {
+        return (
+          <Animal
+            className=".animal .name img"
+            key={item.name}
+            name={item.name}
+            type={item.type}
+            says={item.says}
+            top={item.top}
+            left={item.left}
+          />
+        )
+      })}
+    </main>
+  )
 }
