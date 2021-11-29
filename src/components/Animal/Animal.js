c6c6cdc163b1b9cd737fb7e8c15c86941495181c
karyssa-dandrea <karyssadandrea@gmail.com>
2021-11-29 15:58:24 -0800

    added style to position animals

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Animal(props) {
   return (
     <>
-      <div className="Animal">
+      <div className="Animal" style={{ top: props.top, left: props.left }}>
         <img height="100" src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
         <span>{props.name}</span>
+        <span>{props.top}</span>
+        <span>{props.left}</span>
       </div>
     </>
   )
